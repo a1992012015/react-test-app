e65d0ae703d7445f760a7cd5ba99b152531787cd
刘杰 <mengyehuanyu@qq.com>
2018-01-26 11:34:17 +0800

    Signed-off-by: 刘杰 <mengyehuanyu@qq.com>第三版轮播完善完成

--- src/view/reactSwipeThree/sweiperItem.jsx
+++ src/view/reactSwipeThree/sweiperItem.jsx
@@ -1,58 +1,40 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 export default class SweipeItem extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            widthUl: null,
-        }
     }
-    /*第一次渲染之后*/
-    componentDidMount(){
-        this.onWindowResize();
-        window.addEventListener('resize',this.onWindowResize.bind(this),false);
-    }
-    /*组件完成更新之后*/
-    componentDidUpdate(){
 
-    }
-    /*组件被销毁*/
-    componentWillUnmount() {
-        window.removeEventListener('resize', this.onWindowResize.bind(this))
-    }
-    /*监听浏览器宽度变化*/
-    onWindowResize(){
-        let { threeUl } = this.refs;
-        this.setState({
-            widthUl: threeUl.clientWidth
-        });
-    }
     /*计算需要前往的列表时=>需要清除过度属性的元素*/
-    getNum(listDots){
-        let { list,deviation } = this.props;
-        listDots = deviation?
-            listDots - 1 < 0?list.length - 1:listDots - 1:
-            listDots + 1 >= list.length?0:listDots + 1;
+    getNum(listDots) {
+        let {list, deviation} = this.props;
+        listDots = deviation ?
+            listDots - 1 < 0 ? list.length - 1 : listDots - 1 :
+            listDots + 1 >= list.length ? 0 : listDots + 1;
         return listDots;
     }
+
     /*计算列表的transtion属性*/
-    countTransition(index){
-        let { speed,isFlag,listDots } = this.props;
-        if(isFlag){
-            return index === this.getNum(listDots) || index === listDots?`transform ${speed}s ease`:'none';
-        }else{
-            return index === listDots?`transform ${speed}s ease`:'none';
+    countTransition(index) {
+        let {speed, isFlag, listDots} = this.props;
+        if (isFlag === 0) {
+            return index === listDots ? `transform ${speed}s ease` : 'none';
+        } else if (isFlag === 1) {
+            return index === this.getNum(listDots) || index === listDots ? `transform ${speed}s ease` : 'none';
+        } else if (isFlag === 2) {
+            return 'none';
+        } else if (isFlag === 3) {
+            return `transform ${speed}s ease`;
         }
     }
 
     render() {
-        let { list,listDots } = this.props;
-        let { widthUl } = this.state;
-        let items = list.map((item,index) => {
+        let {list, listDots, contrast, widthUl} = this.props;
+        let items = list.map((item, index) => {
             /*获取需要显示的视图前一个编号*/
-            let numStr = listDots - 1 < 0?list.length -1:listDots -1;
+            let numStr = listDots - 1 < 0 ? list.length - 1 : listDots - 1;
             /*获取视图后面的选项需要的排列序号*/
-            let numEnd = index - listDots >= 0?index - listDots:list.length + index - listDots;
+            let numEnd = index - listDots >= 0 ? index - listDots : list.length + index - listDots;
             let style = {
                 backgroundImage: `url(${item.src})`,
                 backgroundPosition: '50% 50%',
@@ -65,9 +47,9 @@ export default class SweipeItem extends Component {
                 left: '0',
                 right: '0',
                 transition: this.countTransition(index),
-                transform: index === numStr?
-                    `translateZ(0px) translateX(-${widthUl}px)`:
-                    `translateZ(0px) translateX(${widthUl * numEnd}px)`,
+                transform: index === numStr ?
+                    `translateZ(0px) translateX(${-widthUl + contrast}px)` :
+                    `translateZ(0px) translateX(${widthUl * numEnd + contrast}px)`,
             };
             return (
                 <li style={style} key={`threeLi${index}`}/>
@@ -75,7 +57,7 @@ export default class SweipeItem extends Component {
         });
         let styleUl = {
             width: '100%',
-            overflow:'hidden',
+            overflow: 'hidden',
             position: 'absolute',
             top: '0',
             bottom: '0',
@@ -85,7 +67,6 @@ export default class SweipeItem extends Component {
         return (
             <ul
                 style={styleUl}
-                ref='threeUl'
             >
                 {items}
             </ul>
--- src/view/reactSwipeThree/swiperThree.jsx
+++ src/view/reactSwipeThree/swiperThree.jsx
@@ -69,119 +69,185 @@ class SwipeTree extends Component {
             delay: props.delay, //停留的时间
             deviation: true, //滑动的方向
             autoPlayFlag: null, //接收轮播图的计时器
-            isFlag: false,//决定是初始化还是正常运作
-            auto: true, //是否自动开始播放
+            isFlag: 0, //决定是初始化还是正常运作
+            widthUl: null, //保存浏览器宽度数据
+            auto: false, //是否自动开始播放
+            clientStar: null, //鼠标按下时的坐标
+            func: null, //保存鼠标拖动的临时函数
+            resizeFun: null, //保存浏览器监听的临时函数
+            contrast: 0, //保存拖动距离
+
         }
     }
+
     /*第一次渲染之前*/
-    componentWillMount(){
-        let { dots } = this.state;
-        let { listP } = this.props;
+    componentWillMount() {
+        let {dots} = this.state;
+        let {listP} = this.props;
         this.setState({
-            list:this.countList(dots + 1 >= listP.length?0:dots + 1,dots)
+            list: this.countList(dots + 1 >= listP.length ? 0 : dots + 1, dots, true)
         })
     }
+
     /*第一次渲染之后*/
-    componentDidMount(){
-        let { autoPlayFlag,delay,auto } = this.state;
-        if(auto){
+    componentDidMount() {
+        let {autoPlayFlag, delay, auto} = this.state;
+        this.onWindowResize();
+        let resizeFun = () => this.onWindowResize();
+        window.addEventListener('resize', resizeFun, false);
+        if (auto) {
             autoPlayFlag = this.setUpInterval(delay);
             this.setState({
-                autoPlayFlag: autoPlayFlag
+                autoPlayFlag: autoPlayFlag,
+                resizeFun
+            })
+        } else {
+            this.setState({
+                resizeFun
             })
         }
     }
-    /*创建一个计时器并且返回*/
-    setUpInterval(delay){
-        let { listP } = this.props;
-        return setInterval(() =>{
-            let {dots} = this.state;
-            this.goOn(dots + 1 >= listP.length?0:dots + 1,true);
-        },delay * 1000);
+
+    /*组件被销毁*/
+    componentWillUnmount() {
+        let {resizeFun, func} = this.state;
+        document.removeEventListener('mousemove', func, false);
+        window.removeEventListener('resize', resizeFun, false)
     }
+
+    /*监听浏览器宽度变化*/
+    onWindowResize() {
+        let {threeUl} = this.refs;
+        this.setState({
+            widthUl: threeUl.clientWidth
+        });
+    }
+
+    /*创建一个计时器并且返回*/
+    setUpInterval(delay) {
+        let {listP} = this.props;
+        return setInterval(() => {
+            let {dots} = this.state;
+            this.goOn(dots + 1 >= listP.length ? 0 : dots + 1, true);
+        }, delay * 1000);
+    }
+
     /*计算需要显示的列表并且设置*/
-    countList(index,dots,deviation){
-        let { listP } = this.props;
-        let { listDots } = this.state;
+    countList(index, dots, deviation) {
+        let {listP} = this.props;
+        let {listDots, isFlag} = this.state;
         let arr = new Array(3);
         arr[listDots] = listP[dots];
-        if(deviation){
-            arr[listDots + 1 > 2?0:listDots + 1] = listP[index];
-            arr[listDots - 1 < 0?2:listDots - 1] = listP[dots - 1 < 0?listP.length -1:dots - 1];
-        }else{
-            let num = listDots - 1 < 0?2:listDots - 1;
-            arr[num] = listP[index];
-            arr[num - 1 < 0?2:num - 1] = listP[index - 1 < 0?listP.length -1:index - 1];
+        if (isFlag === 0) {
+            arr[listDots + 1 > 2 ? 0 : listDots + 1] = listP[index];
+            arr[listDots - 1 < 0 ? 2 : listDots - 1] = listP[dots - 1 < 0 ? listP.length - 1 : dots - 1];
+        } else {
+            if (deviation) {
+                arr[listDots + 1 > 2 ? 0 : listDots + 1] = listP[index];
+                arr[listDots - 1 < 0 ? 2 : listDots - 1] = listP[index + 1 >= listP.length ? 0 : index + 1];
+            } else {
+                let num = listDots - 1 < 0 ? 2 : listDots - 1;
+                arr[num] = listP[index];
+                arr[num - 1 < 0 ? 2 : num - 1] = listP[index - 1 < 0 ? listP.length - 1 : index - 1];
+            }
         }
         return arr;
     }
+
     /*执行动画*/
+
     /*
     * index: Number 需要前往的页数
     * deviation：Boole 决定前进的方向*/
-    goOn(index,deviation){
-        let { dots,listDots } = this.state;//当前的页数
-        let list = this.countList(index,dots,deviation);
-        listDots = deviation?
-            listDots + 1 > 2?0:listDots + 1:
-            listDots - 1 < 0?2:listDots - 1;
+    goOn(index, deviation) {
+        let {dots, listDots} = this.state;//当前的页数
+        let list = this.countList(index, dots, deviation);
+        listDots = deviation ?
+            listDots + 1 > 2 ? 0 : listDots + 1 :
+            listDots - 1 < 0 ? 2 : listDots - 1;
         this.setState({
             dots: index,
             listDots: listDots,
-            isFlag: true,
+            isFlag: 1,
             list: list,
             deviation
         })
     }
+
     /*跳转需要的下标的视图*/
-    jump(index){
-        let { dots } = this.state;//当前的页数
+    jump(index) {
+        let {dots} = this.state;//当前的页数
         let num = index - dots;
-        if(num > 0){
-            this.goOn(index,true)
-        }else if(num < 0){
-            this.goOn(index,false)
+        if (num > 0) {
+            this.goOn(index, true)
+        } else if (num < 0) {
+            this.goOn(index, false)
         }
     }
+
     /*鼠标移入视图*/
-    setMouseEnter(){
-        console.log('移入');
-        let { autoPlayFlag } = this.state;
+    setMouseEnter() {
+        let {autoPlayFlag} = this.state;
         clearInterval(autoPlayFlag);
     }
+
     /*鼠标移出视图*/
-    setMouseLeave(){
-        console.log('移出');
-        let { delay,auto } = this.state;
-        if(auto){
+    setMouseLeave() {
+        let {delay, auto} = this.state;
+        if (auto) {
             let autoPlayFlag = this.setUpInterval(delay);
             this.setState({
                 autoPlayFlag: autoPlayFlag
             })
         }
     }
+
     /*视图的鼠标按下事件*/
-    setOnMouseDown(event){
-        console.log('按下');
+    setOnMouseDown(event) {
         let e = event || window.event;
-        let a = {'x':e.clientX,'y':e.clientY};
-        console.log(a);
-        document.addEventListener('mousemove',this.setOnMouseMove.bind(this,a),false);
+        let clientStar = {'x': e.clientX, 'y': e.clientY};
+        let func = () => this.setOnMouseMove();
+        this.setState({
+            clientStar: clientStar,
+            func: func,
+        });
+        document.addEventListener('mousemove', func, false);
     }
+
     /*视图鼠标松开事件*/
-    setOnMouseUp(){
-        console.log('松开');
-        document.removeEventListener('mousemove',this.setOnMouseMove,false);
+    setOnMouseUp() {
+        let {func, contrast, widthUl, dots} = this.state;
+        let {listP} = this.props;
+        document.removeEventListener('mousemove', func, false);
+        let cacheContrast = contrast > 0 ? contrast : -contrast;
+        if (cacheContrast > widthUl / 3) {
+            this.setState({
+                contrast: 0,
+                isFlag: 2,
+            });
+            contrast > 0 ?
+                this.goOn(dots - 1 < 0 ? listP.length - 1 : dots - 1, false) :
+                this.goOn(dots + 1 >= listP.length ? 0 : dots + 1, true);
+        } else if (cacheContrast < widthUl / 3) {
+            this.setState({
+                isFlag: 3,
+                contrast: 0,
+            });
+        }
     }
-    /*鼠标按下事件*/
-    setOnMouseMove(event,ops){
-        console.log('拖动');
-        console.log(ops);
-        console.log(event);
-        let e = ops || window.event;
-        let a = {'x':e.clientX,'y':e.clientY};
-        console.log(a);
+
+    /*鼠标按下拖动事件*/
+    setOnMouseMove(event) {
+        let {clientStar} = this.state;
+        let e = event || window.event;
+        let clientEnd = {'x': e.clientX, 'y': e.clientY};
+        let length = clientStar.x - clientEnd.x;
+        this.setState({
+            contrast: -length,
+            isFlag: 2,
+        })
     }
+
     render() {
         /*外层容器的样式*/
         let style = {
@@ -228,8 +294,8 @@ class SwipeTree extends Component {
             bottom: '0',
             marginBottom: '10px'
         };
-        let { dots, list, speed, isFlag, deviation,listDots } = this.state;
-        let { listP } = this.props;
+        let {dots, list, speed, isFlag, deviation, listDots, contrast, widthUl} = this.state;
+        let {listP} = this.props;
         /*显示圆点按钮*/
         let listLi = listP.map((item, index) => {
             /*下面的小圆点的样式*/
@@ -245,32 +311,35 @@ class SwipeTree extends Component {
                 <li
                     style={styleLi}
                     key={`threeLi${index}`}
-                    onClick={()=>this.jump(index)}
+                    onClick={() => this.jump(index)}
                 />
             )
         });
         return (
             <div
                 style={style}
+                ref='threeUl'
                 onMouseEnter={() => this.setMouseEnter()}
                 onMouseLeave={() => this.setMouseLeave()}
-                onMouseDown={(i)=>this.setOnMouseDown(i)}
-                onMouseUp={()=>this.setOnMouseUp()}
+                onMouseDown={(i) => this.setOnMouseDown(i)}
+                onMouseUp={() => this.setOnMouseUp()}
             >
                 <SweipeItem
                     list={list} //需要显示的列表
                     speed={speed} //动画过度的速度
                     listDots={listDots} //需要显示的那一个列表||默认从1开始
                     deviation={deviation} //滑动的方向true向左||false向右
-                    isFlag={isFlag}//决定是初始化还是正常运作
+                    isFlag={isFlag} //决定是初始化还是正常运作
+                    contrast={contrast} //保存鼠标拖动的距离
+                    widthUl={widthUl} //保存容器宽度数据
                 />
                 <span
                     style={Left}
-                    onClick={() => this.goOn(dots + 1 >= listP.length?0:dots + 1,true)}
+                    onClick={() => this.goOn(dots + 1 >= listP.length ? 0 : dots + 1, true)}
                 >&#8249;</span>
                 <span
                     style={Right}
-                    onClick={() => this.goOn(dots - 1 < 0?listP.length - 1:dots - 1,false)}
+                    onClick={() => this.goOn(dots - 1 < 0 ? listP.length - 1 : dots - 1, false)}
                 >&#8250;</span>
                 <ul
                     style={styleUl}
